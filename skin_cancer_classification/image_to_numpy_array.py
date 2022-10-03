d29484c9013edfffbb83e1cb452d31b7603509ca
kriesnawanh98 <kriesnawanh98@gmail.com>
2022-10-03 23:39:29 +0700

    a

--- skin_cancer_classification/image_to_numpy_array.py
+++ skin_cancer_classification/image_to_numpy_array.py
@@ -1,7 +1,5 @@
 from additional import convert_image_to_array
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
 
 X_benign_train, y_benign_train = convert_image_to_array(
     path="./dataset/train/benign/", label="benign")
@@ -28,15 +26,3 @@ np.savez_compressed('X_train.npz', X_train)
 np.savez_compressed('y_train.npz', y_train)
 np.savez_compressed('X_test.npz', X_test)
 np.savez_compressed('y_test.npz', y_test)
-
-# x = 4
-# n_estimators = 2  # 500 trees
-# max_depth = x + 1
-# rnd_clf = RandomForestClassifier(n_estimators=n_estimators,
-#                                  max_depth=max_depth,
-#                                  n_jobs=-1)
-
-# rnd_clf.fit(X_train, y_train)
-
-# print("Predict = ", rnd_clf.predict([X_test[0]]))
-# print("actualnya = ", y_test[0])
